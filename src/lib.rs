ceb5e980098e4d4d8707baad178a603ce6ecf887
WaterlessPiano2 <scderya@gmail.com>
2022-08-07 20:55:50 +0100

    contract deploy works after upgrading solana cli to latest

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,3 @@ pub mod instruction;
 pub mod processor;
 pub mod state;
 pub mod entrypoint;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
